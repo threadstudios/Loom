e81f865c10e87012d00406c6db0dce14c6c5e530
Paul Westerdale <paul@westerdale.me>
2018-05-26 18:41:53 +0100

    bundle assets

--- src/middleware.js
+++ src/middleware.js
@@ -1,13 +1,15 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 const render = require('./render');
 
 module.exports = (Loom) => {
+
+    router.use(express.static('/assets'));
 
     router.get('/*', (req, res) => {
         const page = Loom.content.get(req.originalUrl);
         const html = Loom.render(page);
         res.send(html);
-        
     });
 
     return router;
